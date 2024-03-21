82cc368218c4afcff8b9e89d395277d27a1c3b1a
Felipe Barbugian Borges da Cunha <127468188+febarbugian@users.noreply.github.com>
2024-03-21 18:00:06 -0300

    Add files via upload

---
+++ import requests.py
@@ -0,0 +1,34 @@
+import requests
+from openpyxl import Workbook
+
+def obter_ultima_operacao(cliente):
+    url = f"http://10.204.5.28:8080/ultima-operacao?cliente={cliente}"
+    resposta = requests.get(url)
+    dados = resposta.json()
+    return dados
+
+clientes = [11994, 95973]  
+
+# Criar uma nova pasta de trabalho do Excel
+wb = Workbook()
+# Selecionar a primeira planilha (por padrão, é Sheet)
+ws = wb.active
+
+# Adicionar cabeçalhos
+ws.append(["Cliente", "Data última operação na BVSP", "Data última operação na BMF"])
+
+for cliente in clientes:
+    informacoes = obter_ultima_operacao(cliente)
+    # Verificar se as informações não são NULL antes de processar
+    if informacoes is not None:
+        # Remover o "T" das datas
+        data_bvsp = informacoes.get('DT_ULT_OPER_BVSP', '').split("T")[0]
+        data_bmf = informacoes.get('DT_ULT_OPER_BMF', '').split("T")[0]
+        # Adicionar informações do cliente à planilha
+        ws.append([cliente, data_bvsp, data_bmf])
+
+# Salvar a pasta de trabalho
+wb.save("informacoes_ultima_operacao.xlsx")
+
+import os
+print("O arquivo foi salvo em:", os.getcwd())
